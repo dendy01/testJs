5a951a345e6576f98d58c35813abd72a992321c6
dendy01 <biomannumberone@gmail.com>
2023-06-22 01:43:27 +0300

    add export

--- script/message.js
+++ script/message.js
@@ -21,6 +21,25 @@ function abs(a) {
 	return a < 0 ? -a : a;
 }
 
+const isContinuousSequence = (arr) => {
+  let isTrue = 0;
+  let isFalse = 0;
+
+  for(let i = 0, j = 1; i < arr.length && j < arr.length; i++, j++) {
+
+    if((Math.abs(arr[j]) - Math.abs(arr[i])) === 1 || (Math.abs(arr[j]) - Math.abs(arr[i])) === -1) {
+      isTrue += 1;
+    } else {
+      return false;
+    }
+  }
+
+  if(isTrue > isFalse) {
+    return true;
+  }
+}
+
+
 const PI = 3.14;
 
-export {sayHi, getSum, abs, PI, Person};
+export {sayHi, getSum, abs, PI, Person, isContinuousSequence};
--- script/script.js
+++ script/script.js
@@ -1,19 +1,3 @@
-const isContinuousSequence = (arr) => {
-  let isTrue = 0;
-  let isFalse = 0;
-
-  for(let i = 0, j = 1; i < arr.length && j < arr.length; i++, j++) {
-
-    if((Math.abs(arr[j]) - Math.abs(arr[i])) === 1 || (Math.abs(arr[j]) - Math.abs(arr[i])) === -1) {
-      isTrue += 1;
-    } else {
-      return false;
-    }
-  }
-
-  if(isTrue > isFalse) {
-    return true;
-  }
-}
+import {isContinuousSequence} from './message.js';
 
 console.log(isContinuousSequence([10, 11, 12]));
